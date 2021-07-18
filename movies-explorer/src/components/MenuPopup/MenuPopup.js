caec95ee71c3fa03f47f7b20efe76fecbfa7a075
Anna Link <annlink2212@gmail.com>
2021-07-18 12:16:22 +0800

    3review

--- movies-explorer/src/components/MenuPopup/MenuPopup.js
+++ movies-explorer/src/components/MenuPopup/MenuPopup.js
@@ -6,9 +6,11 @@ function MenuPopup() {
             <div className="menu__container menu__container_visible">
                 <button type="button" className="menu__close-btn"></button>
                 <div className="menu__links">
-                    <Link to="/" className="menu__link menu__main-link">Главная</Link>
-                    <Link to="/movies" className="menu__link  menu__movies-link">Фильмы</Link>
-                    <Link to="/saved-movies" className="menu__link menu__saved-link">Сохранённые фильмы</Link>
+                    <div className="menu__div">
+                        <Link to="/" className="menu__link menu__main-link">Главная</Link>
+                        <Link to="/movies" className="menu__link  menu__movies-link">Фильмы</Link>
+                        <Link to="/saved-movies" className="menu__link menu__saved-link">Сохранённые фильмы</Link>
+                    </div>
                     <Link to="/profile" className="menu__profile-btn">
                         <div className="menu__icon"></div>Аккаунт
                     </Link>
